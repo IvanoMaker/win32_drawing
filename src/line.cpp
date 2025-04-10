6736fd3ad1219da62e926a7688a79d0e89240f50
Evan M <105332257+IvanoMaker@users.noreply.github.com>
2025-04-10 00:02:29 -0400

    Update line.cpp

--- src/line.cpp
+++ src/line.cpp
@@ -5,7 +5,7 @@
 Line::Line(fPoint start, fPoint end, int width, int color)
     : a(start), b(end), width(width), color(color) {}
 
-Line::draw(HDC hdc) {
+void Line::draw(HDC hdc) {
     HPEN pen = CreatePen(PS_SOLID, width, (COLORREF)color);
     HPEN oldPen = (HPEN)SelectObject(hdc, pen);
     MoveToEx(hdc, a.x, a.y, NULL);
